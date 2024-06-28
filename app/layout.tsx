f31fd141f2e9e9dea5ac2050c39feaa14ba03338
shinnthant2004 <shinthantequi@gmail.com>
2024-06-28 20:03:51 +0630

    feat: add theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/providers/ThemeProvider"
 import Header from "@/components/layouts/header"
 import "./globals.css"
 
@@ -18,9 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
