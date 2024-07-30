36281e9bd4881fcf951219b4cedae935c706522e
shinnthant2004 <shinthantequi@gmail.com>
2024-07-30 16:25:51 +0630

    feat(app): implement animation in contact links

--- components/sections/ContactSection.tsx
+++ components/sections/ContactSection.tsx
@@ -76,10 +76,14 @@ export default function ContactSection() {
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Contact me on linkedin"
-              className="rounded-full flex items-center gap-2 border py-[1px] px-4"
+              className="rounded-full group hover:border-accentColor flex items-center gap-2 border py-[1px] px-4"
             >
-              <div className="font-semibold dark:text-white">in</div>
-              <div className="text-xs dark:text-white">Linkedin</div>
+              <div className="font-semibold group-hover:scale-105 dark:text-white">
+                in
+              </div>
+              <div className="text-xs dark:text-white group-hover:scale-105">
+                Linkedin
+              </div>
             </Link>
 
             <Link
@@ -87,7 +91,7 @@ export default function ContactSection() {
               aria-label="Contact me on Twitter"
               target="_blank"
               rel="noopener noreferrer"
-              className="rounded-full flex items-center gap-2 border py-[5px] px-4"
+              className="rounded-full group hover:border-accentColor flex items-center gap-2 border py-[5px] px-4"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -95,7 +99,7 @@ export default function ContactSection() {
                 height="15"
                 viewBox="0 0 24 24"
                 strokeWidth="1.5"
-                className="stroke-black dark:stroke-white"
+                className="stroke-black group-hover:scale-105 dark:stroke-white"
                 fill="none"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -104,7 +108,9 @@ export default function ContactSection() {
                 <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
                 <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
               </svg>
-              <div className="text-xs dark:text-white">Twitter</div>
+              <div className="text-xs dark:text-white group-hover:scale-105">
+                Twitter
+              </div>
             </Link>
 
             <Link
@@ -112,7 +118,7 @@ export default function ContactSection() {
               aria-label="Contact me on Github"
               target="_blank"
               rel="noopener noreferrer"
-              className="rounded-full flex items-center gap-2 border py-[5px] px-4"
+              className="rounded-full group flex transition-all items-center gap-2 hover:border-accentColor border py-[5px] px-4"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -120,7 +126,7 @@ export default function ContactSection() {
                 height="15"
                 viewBox="0 0 24 24"
                 strokeWidth="1.5"
-                className="stroke-black dark:stroke-white"
+                className="stroke-black group-hover:scale-105 dark:stroke-white"
                 fill="none"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -128,7 +134,9 @@ export default function ContactSection() {
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
               </svg>
-              <div className="text-xs dark:text-white">Github</div>
+              <div className="text-xs dark:text-white group-hover:scale-105">
+                Github
+              </div>
             </Link>
           </div>
           <div className="flex flex-col items-center">
